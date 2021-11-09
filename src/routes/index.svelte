6935da6b3b64a201fc1321bbc89107f701fa3031
Nate Sales <nate@natesales.net>
2021-11-09 01:21:35 -0800

    fix: add under construction page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,29 @@
 <main>
     <h1>Under Construction</h1>
+    <p>This is a beta deployment. See <a href="https://packetframe.com">packetframe.com</a> for the production site.</p>
 </main>
 
-<style>
+<style global>
     main {
         display: flex;
         align-items: center;
         flex-direction: column;
+        color: white;
     }
 
-    h1 {
-        color: white;
+    html, body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+        background-color: #040404;
+    }
+
+    ::selection {
+        background: #b03ac6;
+    }
+
+    ::-moz-selection {
+        background: #b03ac6;
     }
 </style>
