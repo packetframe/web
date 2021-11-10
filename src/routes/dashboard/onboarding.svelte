8535647e22869e2bdd840184a9f8edc5e517c176
Nate Sales <nate@natesales.net>
2021-11-09 20:31:52 -0800

    feat: add api integration for domain onboarding

--- src/routes/dashboard/onboarding.svelte
+++ src/routes/dashboard/onboarding.svelte
@@ -3,6 +3,36 @@
     import Card from "../../components/Card";
     import Button from "../../components/Button";
     import Title from "../../components/Title";
+
+    let zone;
+    let zoneError;
+
+    function submit() {
+        zoneError = ""
+        fetch("http://localhost:8080/dns/zones", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "zone": zone,
+            })
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    window.location = "/dashboard/login"
+                }
+                return response.json()
+            })
+            .then((data) => {
+                if (data.success) {
+                    window.location = "/dashboard/dns"
+                } else {
+                    zoneError = "Invalid domain"
+                }
+            })
+    }
 </script>
 
 <main>
@@ -11,7 +41,7 @@
     </Title>
     <Card title="Add a domain">
         <p>Make sure your domain's nameservers are configured to <b>ns1.packetframe.com</b> and <b>ns2.packetframe.com</b>.</p>
-        <Input fixErrorHeight={false} label="Domain" placeholder="example.com" type="text"/>
-        <Button style="margin-top: 18px" variant="secondary">Add</Button>
+        <Input bind:value={zone} bind:error={zoneError} fixErrorHeight={false} label="Domain" placeholder="example.com" type="text"/>
+        <Button style="margin-top: 18px" variant="secondary" on:click={submit}>Add</Button>
     </Card>
 </main>
