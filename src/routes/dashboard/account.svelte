abb8d02fcd12da4537ebe261cd18ed95c430179d
Nate Sales <nate@natesales.net>
2021-11-13 19:55:05 -0800

    feat: add change password call

--- src/routes/dashboard/account.svelte
+++ src/routes/dashboard/account.svelte
@@ -25,6 +25,53 @@
                 }
             })
     }
+
+    function changePassword() {
+        if (password !== passwordRepeat) {
+            passwordRepeatError = "Passwords must match"
+            return
+        }
+        passwordError = ""
+        passwordRepeat = ""
+
+        fetch("http://localhost:8080/user/password", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({password: password})
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    window.location = "/dashboard/login"
+                }
+                return response.json()
+            })
+            .then((data) => {
+                if (data.success) {
+                    alert("Password changed successfully. Please log in again.")
+                    window.location = "/dashboard/login"
+                } else {
+                    if (data.data) {
+                        for (const reason of data.data.reason) {
+                            if (reason.Tag === "min") {
+                                passwordError = `Password must be at least ${reason.Value} characters`
+                            } else if (reason.Tag == "max") {
+                                passwordError = `Password must be less than ${reason.Value + 1} characters`
+                            }
+                        }
+                    } else {
+                        alert(data.message)
+                    }
+                }
+            })
+    }
+
+    let password;
+    let passwordError;
+    let passwordRepeat;
+    let passwordRepeatError;
 </script>
 
 <main>
@@ -32,13 +79,12 @@
         <div slot="header">Account</div>
     </Title>
     <Card title="Change Password">
-        <Input fixErrorHeight={false} label="Current password" placeholder="Enter password..." style="margin-bottom: 10px" type="password"/>
-        <Input fixErrorHeight={false} label="New password" placeholder="Enter password..." style="margin-bottom: 10px" type="password"/>
-        <Input fixErrorHeight={false} label="Repeat new password" placeholder="Enter password..." type="password"/>
-        <Button style="margin-top: 18px" variant="secondary">Save</Button>
+        <Input bind:error={passwordError} bind:value={password} fixErrorHeight={false} label="New password" placeholder="Enter password..." style="margin-bottom: 10px" type="password"/>
+        <Input bind:error={passwordRepeatError} bind:value={passwordRepeat} fixErrorHeight={false} label="Repeat new password" placeholder="Enter password..." type="password"/>
+        <Button on:click={changePassword} style="margin-top: 18px" variant="secondary">Save</Button>
     </Card>
     <Card title="Delete Account">
         <p>Deleting your account is <b>permanent</b> and cannot be undone.</p>
-        <Button style="margin-top: 18px" variant="filled" danger on:click={deleteUser}>Delete Account</Button>
+        <Button danger on:click={deleteUser} style="margin-top: 18px" variant="filled">Delete Account</Button>
     </Card>
 </main>
