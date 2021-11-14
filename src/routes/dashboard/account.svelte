863947cb90f872f16161887af851f945d62f7184
Nate Sales <nate@natesales.net>
2021-11-13 19:49:11 -0800

    feat: add delete user call

--- src/routes/dashboard/account.svelte
+++ src/routes/dashboard/account.svelte
@@ -3,6 +3,28 @@
     import Card from "../../components/Card";
     import Button from "../../components/Button";
     import Title from "../../components/Title";
+
+    function deleteUser() {
+        if (!confirm("Are you sure you want to delete your account? This action is irreversible.")) {
+            return
+        }
+
+        fetch("http://localhost:8080/user/delete", {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    window.location = "/"
+                } else {
+                    alert(data.message)
+                }
+            })
+    }
 </script>
 
 <main>
@@ -17,6 +39,6 @@
     </Card>
     <Card title="Delete Account">
         <p>Deleting your account is <b>permanent</b> and cannot be undone.</p>
-        <Button style="margin-top: 18px" variant="filled" danger>Delete Account</Button>
+        <Button style="margin-top: 18px" variant="filled" danger on:click={deleteUser}>Delete Account</Button>
     </Card>
 </main>
