a967d629c414b3e29f3bfb08ef601d6586fd5b1c
Nate Sales <nate@natesales.net>
2021-11-23 00:47:16 -0800

    feat: start parallax

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,61 @@
 <main class="index-wrapper">
-    <img class="construction" src="construction.png" alt="Under Construction">
+    <img alt="Under Construction Cloud" class="gradient-cloud" src="construction_cloud.png">
+    <img alt="Black" class="black" src="black.png">
+<!--        <img alt="Under Construction" class="paper" src="construction_transparent.png">-->
     <p class="footer">&copy; Packetframe 2021</p>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Nibh venenatis cras sed felis eget velit. Libero justo laoreet sit amet. Hac habitasse platea dictumst quisque. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Lacus sed turpis tincidunt id. Dui vivamus arcu felis bibendum ut tristique et egestas. At tempor commodo ullamcorper a lacus vestibulum sed arcu. Euismod quis viverra nibh cras pulvinar mattis nunc sed blandit. Eu nisl nunc mi ipsum faucibus vitae aliquet. Egestas erat imperdiet sed euismod nisi porta. Dui faucibus in ornare quam viverra orci sagittis. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam. Mollis aliquam ut porttitor leo a. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Euismod nisi porta lorem mollis aliquam. Feugiat pretium nibh ipsum consequat nisl vel.
+        Sed libero enim sed faucibus turpis in eu mi bibendum.
+
+        Egestas purus viverra accumsan in nisl nisi scelerisque eu. Ornare aenean euismod elementum nisi quis eleifend. Massa vitae tortor condimentum lacinia quis vel eros donec. Quis lectus nulla at volutpat diam ut venenatis tellus in. Velit laoreet id donec ultrices tincidunt arcu non sodales. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Quisque egestas diam in arcu cursus euismod quis. Et leo duis ut diam quam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Quis hendrerit dolor magna eget est. Odio facilisis mauris sit amet massa vitae tortor. Massa tincidunt dui ut ornare lectus. Quis auctor elit sed vulputate mi sit amet mauris commodo. Molestie nunc non blandit massa enim nec. Tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Sit amet tellus cras adipiscing enim. Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Dui sapien
+        eget mi proin sed libero. Facilisis mauris sit amet massa vitae tortor condimentum lacinia.
+
+        Venenatis cras sed felis eget velit aliquet sagittis. Congue nisi vitae suscipit tellus mauris a diam maecenas. Ornare massa eget egestas purus viverra accumsan. Eget nullam non nisi est. Aliquam ut porttitor leo a diam sollicitudin. Mollis nunc sed id semper risus in. Tellus elementum sagittis vitae et leo duis ut diam quam. Accumsan lacus vel facilisis volutpat est. Arcu cursus vitae congue mauris rhoncus aenean vel elit. Duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Commodo viverra maecenas accumsan lacus vel facilisis. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Amet nisl purus in mollis nunc sed. Non diam phasellus vestibulum lorem sed risus ultricies. Faucibus turpis in eu mi bibendum neque. Id aliquet risus feugiat in. Aliquam sem fringilla ut morbi tincidunt. Velit ut tortor pretium viverra suspendisse potenti nullam ac tortor. Quis imperdiet massa tincidunt nunc pulvinar.
+
+        Consequat nisl vel pretium lectus quam id leo in. Lobortis elementum nibh tellus molestie nunc non blandit. Duis convallis convallis tellus id interdum velit laoreet id. Feugiat vivamus at augue eget arcu dictum varius. Sed blandit libero volutpat sed. Diam in arcu cursus euismod quis viverra nibh cras. Consectetur purus ut faucibus pulvinar elementum. Hac habitasse platea dictumst vestibulum. Rhoncus urna neque viverra justo nec. Sit amet consectetur adipiscing elit duis. Sapien pellentesque habitant morbi tristique senectus et netus et. Dolor sit amet consectetur adipiscing elit. Id diam vel quam elementum pulvinar etiam. Accumsan sit amet nulla facilisi. Tortor posuere ac ut consequat semper viverra nam libero justo.
+
+        Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam. At imperdiet dui accumsan sit amet nulla facilisi morbi. Eget egestas purus viverra accumsan in nisl nisi scelerisque eu. Nunc non blandit massa enim nec dui nunc. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Scelerisque eleifend donec pretium vulputate sapien nec. Id eu nisl nunc mi ipsum faucibus vitae. Donec massa sapien faucibus et molestie ac feugiat. Egestas diam in arcu cursus. Enim ut sem viverra aliquet eget.
+
+        Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Nulla porttitor massa id neque aliquam. Adipiscing enim eu turpis egestas pretium. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Arcu cursus vitae congue mauris rhoncus aenean vel. Risus nullam eget felis eget nunc lobortis. Sodales ut etiam sit amet nisl. Nibh venenatis cras sed felis eget velit aliquet. Eget gravida cum sociis natoque penatibus et. Odio facilisis mauris sit amet massa vitae tortor. Habitant morbi tristique senectus et netus et. Senectus et netus et malesuada fames ac turpis. Gravida rutrum quisque non tellus orci. Justo eget magna fermentum iaculis eu non.
+    </p>
 </main>
 
 <style global>
     main.index-wrapper {
+        background-image: url("construction_transparent.png");
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        position: relative;
         width: 100%;
         height: 100%;
-        position: relative;
-        overflow: hidden;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        perspective: 1px;
+        perspective-origin: 0 0;
+        background-color: #000000;
     }
 
-    img.construction {
+    img.black {
         width: 100%;
-        height: 100%;
-        object-fit: cover;
+        transform-origin: 0 0;
+        transform: translateX(-50%) translateY(200%) translateZ(-2px) scaleX(4) scaleY(1);
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        z-index: 0;
+    }
+
+    img.gradient-cloud {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        z-index: 1;
     }
 
     html, body {
