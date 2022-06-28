38ff72f0ba4b6099d37fc79fc3af4fad2dbce77e
Nate Sales <nate@natesales.net>
2022-06-27 20:39:10 -0700

    feat: add icons to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,39 @@
-<footer>&copy; Packetframe 2022</footer>
+<script>
+    import {onMount} from "svelte";
+
+    let email;
+    onMount(() => {
+        email = window.atob("aW5mb0BwYWNrZXRmcmFtZS5jb20="); // encoded contact email
+    })
+</script>
+
+<main>
+    <div class="icons">
+        <a href="mailto:{email}"><i class="fa fa-envelope fa-xl"></i></a>
+        <a href="https://github.com/packetframe"><i class="fab fa-github fa-xl"></i></a>
+        <a href="https://twitter.com/packetframe"><i class="fab fa-twitter fa-xl"></i></a>
+    </div>
+
+    <p>&copy; Packetframe 2022</p>
+</main>
 
 <style>
-    footer {
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
         margin: 15px;
         color: white;
-        text-align: center;
+    }
+
+    a {
+        margin: 5px;
+        color: white;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #dd00ff;
     }
 </style>
