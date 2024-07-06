d0bf23d79351d89c8cd05f07820e9cd422f762af
Nate Sales <nate@natesales.net>
2024-07-06 00:47:40 -0400

    fix: node map

---
+++ src/nodes.js
@@ -0,0 +1,169 @@
+export let nodes = [{
+	'name': 'fmt2',
+	'id': 10,
+	'pdb': 547,
+	'geo': [37.471809, -121.920111],
+	'pdbname': 'Hurricane Electric Fremont 2'
+}, {
+	'name': 'syd1',
+	'id': 11,
+	'pdb': 1605,
+	'geo': [-33.911776, 151.192942],
+	'pdbname': 'Equinix SY3 - Sydney'
+}, {
+	'name': 'sin1',
+	'id': 12,
+	'pdb': 4015,
+	'geo': [1.337098, 103.892998],
+	'pdbname': 'Racks Central'
+}, {
+	'name': 'hgk1',
+	'id': 13,
+	'pdb': 170,
+	'geo': [22.365581, 114.11932],
+	'pdbname': 'Equinix HK1 - Hong Kong'
+}, {
+	'name': 'mia1',
+	'id': 14,
+	'pdb': 15,
+	'geo': [25.782648, -80.193157],
+	'pdbname': 'Equinix MI1 - Miami, NOTA'
+}, {
+	'name': 'jnb1',
+	'id': 15,
+	'pdb': 6837,
+	'geo': [-26.074142, 28.278459],
+	'pdbname': 'Teraco Johannesburg Bredell Campus'
+}, { 'name': 'sju1', 'id': 16, 'pdb': 7291, 'geo': [18.465015, -66.097022], 'pdbname': 'NetWave PR' }, {
+	'name': 'sea1',
+	'id': 17,
+	'pdb': 5183,
+	'geo': [47.49312, -122.293712],
+	'pdbname': 'WowRack'
+}, {
+	'name': 'jnb2',
+	'id': 18,
+	'pdb': 6837,
+	'geo': [-26.074142, 28.278459],
+	'pdbname': 'Teraco Johannesburg Bredell Campus'
+}, {
+	'name': 'lys1',
+	'id': 20,
+	'pdb': 724,
+	'geo': [45.729237, 4.843612],
+	'pdbname': 'hosTELyon'
+}, {
+	'name': 'yvr1',
+	'id': 21,
+	'pdb': 8060,
+	'geo': [49.259393, -123.029619],
+	'pdbname': 'Cologix VAN3'
+}, {
+	'name': 'trf1',
+	'id': 23,
+	'pdb': 5463,
+	'geo': [59.172097, 10.208795],
+	'pdbname': 'Terrahost K10 Sandefjord'
+}, {
+	'name': 'eze1',
+	'id': 25,
+	'pdb': 1550,
+	'geo': [-34.590248, -58.467164],
+	'pdbname': 'Cirion Buenos Aires - BUE1'
+}, {
+	'name': 'lim1',
+	'id': 26,
+	'pdb': 7320,
+	'geo': [-12.087361, -76.97314],
+	'pdbname': 'Equinix LM1 - Lima'
+}, {
+	'name': 'mex1',
+	'id': 27,
+	'pdb': 4592,
+	'geo': [19.628098, -99.152562],
+	'pdbname': 'KIO México 5 (MEX5)'
+}, {
+	'name': 'bog1',
+	'id': 30,
+	'pdb': 7697,
+	'geo': [4.754936, -74.064615],
+	'pdbname': 'Cirion Bogota - BOG1'
+}, {
+	'name': 'fra2',
+	'id': 33,
+	'pdb': 108,
+	'geo': [50.096962, 8.630057],
+	'pdbname': 'Telehouse - Frankfurt'
+}, {
+	'name': 'chi1',
+	'id': 34,
+	'pdb': 440,
+	'geo': [41.876212, -87.631453],
+	'pdbname': 'CoreSite - Chicago (CH1)'
+}, {
+	'name': 'hnl1',
+	'id': 35,
+	'pdb': 2526,
+	'geo': [21.335678, -157.917855],
+	'pdbname': 'DRFortress HNL'
+}, {
+	'name': 'phx1',
+	'id': 36,
+	'pdb': 1917,
+	'geo': [33.395544, -111.97012],
+	'pdbname': 'EdgeConneX Phoenix (EDCPHX01)'
+}, {
+	'name': 'pdx1',
+	'id': 37,
+	'pdb': 275,
+	'geo': [45.521366, -122.680792],
+	'pdbname': 'The Pittock'
+}, {
+	'name': 'amr1',
+	'id': 38,
+	'pdb': 6888,
+	'geo': [16.421067, 80.55841],
+	'pdbname': 'Pi Datacenters Amaravati'
+}, {
+	'name': 'iad1',
+	'id': 39,
+	'pdb': 668,
+	'geo': [38.950631, -77.364541],
+	'pdbname': 'CoreSite - Reston (VA1)'
+}, {
+	'name': 'fra1',
+	'id': 40,
+	'pdb': 58,
+	'geo': [50.11967, 8.73463],
+	'pdbname': 'Digital Realty Frankfurt FRA1-16'
+}, {
+	'name': 'zrh1',
+	'id': 44,
+	'pdb': 336,
+	'geo': [47.38338, 8.495621],
+	'pdbname': 'colozueri.ch Zurich'
+}, {
+	'name': 'ams1',
+	'id': 45,
+	'pdb': 1826,
+	'geo': [52.717249, 6.19872],
+	'pdbname': 'Serverius DC2'
+}, {
+	'name': 'tor1',
+	'id': 47,
+	'pdb': 2206,
+	'geo': [43.650854, -79.361951],
+	'pdbname': 'Equinix TR2 - Toronto'
+}, {
+	'name': 'dvn1',
+	'id': 48,
+	'pdb': 10411,
+	'geo': [41.550323, -90.482459],
+	'pdbname': 'Bluebird Quad Cities Data Center'
+}, {
+	'name': 'gru2',
+	'id': 49,
+	'pdb': 165,
+	'geo': [-23.497589, -46.814577],
+	'pdbname': 'Equinix SP4 - São Paulo'
+}];
--- src/routes/network.svelte
+++ src/routes/network.svelte
@@ -1,90 +1,81 @@
 <script>
-    import {onMount} from "svelte";
-    import Footer from "../components/Footer.svelte";
-    import Navbar from "../components/Navbar/Navbar.svelte";
+	import { onMount } from 'svelte';
+	import Footer from '../components/Footer.svelte';
+	import Navbar from '../components/Navbar/Navbar.svelte';
+	import { nodes } from '../nodes.js';
 
-    let nodes = [];
-    onMount(() => {
-        let netMap = L.map("netmap").setView([0, 0], 1);
-        let CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 19
-        });
-        CartoDB_DarkMatter.addTo(netMap)
+	onMount(() => {
+		let netMap = L.map('netmap').setView([0, 0], 1);
+		let CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+			subdomains: 'abcd',
+			maxZoom: 19
+		});
+		CartoDB_DarkMatter.addTo(netMap);
 
-        let icon = new L.Icon({
-            iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
-        })
+		let icon = new L.Icon({
+			iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
 
-        fetch("https://packetframe.com/nodes.json")
-            .then(resp => resp.json())
-            .then(n => {
-                nodes = n
-                for (const i in nodes) {
-                    fetch("https://www.peeringdb.com/api/fac/" + nodes[i]["pdb"])
-                        .then(resp => resp.json())
-                        .then(data => {
-                            L.marker([data.data[0].latitude, data.data[0].longitude], {icon: icon})
-                                .addTo(netMap)
-                                .bindPopup(`
-                                    <b>${nodes[i]["name"].toUpperCase()} (${nodes[i]["id"]})</b>
-                                    <br>
-                                    <a href='https://peeringdb.com/fac/${nodes[i]['pdb']}'>${data.data[0].name}</a>
-                                `)
-                        })
-                }
-            })
-
-    })
+		for (const i in nodes) {
+			let node = nodes[i];
+			L.marker(node.geo, { icon: icon })
+				.addTo(netMap)
+				.bindPopup(`<b>${node.name.toUpperCase()} (${node.id})</b>
+												<br>
+												<a href='https://peeringdb.com/fac/${node.pdb}'>${node.pdbname}</a>`);
+		}
+	});
 </script>
 
 <main>
-    <Navbar homepage/>
-    <br>
+	<Navbar homepage />
+	<br>
 
-    <div class="body">
-        <h1>Packetframe Network</h1>
+	<div class="body">
+		<h1>Packetframe Network</h1>
 
-        <p>
-            Packetframe operates it's own global anycast network built on donations from the <a href="/community">community</a>.
-            We can deploy on anything from a small virtual machine to spare rack space, so if you're interested in
-            sponsoring a node, please reach out!
-        </p>
+		<p>
+			Packetframe operates it's own global anycast network built on donations from the <a
+			href="/community">community</a>.
+			We can deploy on anything from a small virtual machine to spare rack space, so if you're interested in
+			sponsoring a node, please reach out!
+		</p>
 
-        <h2>Peering with Packetframe</h2>
-        <p>
-            Packetframe has an open peering policy. See <a href="https://peeringdb.com/asn/968">AS968 on PeeringDB</a>
-            for more information. AS968 routes are also available at all <a href="https://as34553.net">AS34553</a>
-            locations.
-        </p>
+		<h2>Peering with Packetframe</h2>
+		<p>
+			Packetframe has an open peering policy. See <a href="https://peeringdb.com/asn/968">AS968 on PeeringDB</a>
+			for more information. AS968 routes are also available at all <a href="https://as34553.net">AS34553</a>
+			locations.
+		</p>
 
-        <p>Each Packetframe node adds a community to indicate which PoP it was originated from:</p>
-        <table>
-            <thead>
-            <tr>
-                <th>Node</th>
-                <th>Community</th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each nodes as node}
-                <tr>
-                    <td>{node.name.toUpperCase()}</td>
-                    <td>968:33:{node.id}</td>
-                </tr>
-            {/each}
-            </tbody>
-        </table>
-    </div>
+		<div id="netmap"></div>
 
-    <div id="netmap"></div>
+		<p>Each Packetframe node announces it's anycast routes tagged with a BGP community to indicate which PoP it was
+			originated from:</p>
+		<table>
+			<thead>
+			<tr>
+				<th>Node</th>
+				<th>Community</th>
+			</tr>
+			</thead>
+			<tbody>
+			{#each nodes as node}
+				<tr>
+					<td>{node.name.toUpperCase()}</td>
+					<td>968:33:{node.id}</td>
+				</tr>
+			{/each}
+			</tbody>
+		</table>
+	</div>
 
-    <Footer/>
+	<Footer />
 </main>
 
 <style>
